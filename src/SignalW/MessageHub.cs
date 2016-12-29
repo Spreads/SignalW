6a56b93b002623a47408fc82104cb87101dcc793
Victor Baybekov <vbaybekov@gmail.com>
2016-12-29 03:48:52 +0300

    Add MessageHub

---
+++ src/SignalW/MessageHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataSpreads.SignalW
+{
+    public class MessageHub : Hub
+    {
+        public override async Task OnReceiveAsync(MemoryStream payload) {
+            object message = payload.ReadJsonMessage();
+            // dispose as soon as it is no longer used becasue it uses pooled buffers inside
+            payload.Dispose();
+
+            // dynamic will dispatch to the correct method
+            dynamic dynMessage = message;
+            await OnReceiveAsync(dynMessage);
+        }
+
+        public virtual async Task OnReceiveAsync(IMessage message) {
+            return;
+        }
+    }
+}
