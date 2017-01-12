754fb7d56ba45b7fea0cc9b6595998e9751ade2b
Victor Baybekov <vbaybekov@gmail.com>
2017-01-12 04:44:02 +0300

    Update version and packing script

---
+++ nuget/local_pack.bat
@@ -0,0 +1,19 @@
+@echo off
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+REM set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "fullstamp=%YY%%MM%%DD%%HH%%Min%"
+REM echo datestamp: "%datestamp%"
+REM echo timestamp: "%timestamp%"
+REM echo fullstamp: "%fullstamp%"
+
+set "build=build%fullstamp%"
+echo build: "%build%"
+
+dotnet pack ..\src\SignalW.Client -c DEBUG -o C:\tools\LocalNuget --version-suffix "%build%"
+
+dotnet pack ..\src\SignalW -c DEBUG -o C:\tools\LocalNuget --version-suffix "%build%"
+
+pause
